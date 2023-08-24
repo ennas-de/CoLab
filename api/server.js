6e16d66482c6e31800f585206469295f2c56d193
ennas-de <dev.abdulhakeem@gmail.com>
2023-08-24 09:57:41 +0100

    update port to be dynamic

--- api/server.js
+++ api/server.js
@@ -69,7 +69,7 @@ app.use("*", (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.POST || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
